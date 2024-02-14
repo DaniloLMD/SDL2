aa3cf1a328bcab452aac65c766a77934ecdf7728
Danilo <danilolmd04@gmail.com>
2024-02-13 21:59:44 -0300

    Dino Game Done

--- Dino-Dinosaur-Game/dino.cpp
+++ Dino-Dinosaur-Game/dino.cpp
@@ -3,6 +3,9 @@
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+
+#include <unistd.h>
 
 using namespace std;
 
@@ -19,8 +22,9 @@ using namespace std;
 const char* SPRITES_FOLDER = "sprites/";
 
 bool quit = false; //booleano que controla o evento do botao de fechar o programa
+bool gameOver = false; //booleano que controla se houve colisao
 
-const double GRAVITY = 0.05; //força que puxa o dinossauro pra baixo
+const double GRAVITY = 0.1; //força que puxa o dinossauro pra baixo
 
 const int SCREEN_WIDTH = 800; //largura da janela principal em pixels
 const int SCREEN_HEIGHT = 400; //altura da janela principal em pixels
@@ -43,12 +47,21 @@ SDL_Event event;
 SDL_Texture *dinoTexture = NULL;
 SDL_Texture* dinoTexture1 = NULL;
 SDL_Texture* dinoTexture2 = NULL;
+SDL_Texture* dinoDuckingTexture = NULL;
+SDL_Texture* dinoDucking2Texture = NULL;
 
 SDL_Texture* groundTexture1 = NULL;
 SDL_Texture* groundTexture2 = NULL;
 SDL_Texture* ptera1Texture = NULL;
 SDL_Texture* ptera2Texture = NULL;
+SDL_Texture* gameOverTexture = NULL;
+SDL_Texture* replayTexture = NULL;
 
+//------------ fontes ---------------------
+const char* FONT_PATH = "fonts/aakar-medium.ttf";
+const int FONT_SIZE = 28; //tamanho da fonte em pixels
+
+TTF_Font* font = NULL;
 
 
 //----------------  Variaveis do Dino ----------------//
@@ -57,9 +70,17 @@ int dinoY = GROUND_HEIGHT;
 
 double dinoYVelocity = 0.0;
 bool isJumping = false;
+bool isDucking = false;
 
-const double JUMP_FORCE = 4.0; //força de subida do pulo
+const double NORMAL_JUMP_FORCE = 6.0; //força de subida do pulo
+const double DUCKING_JUMP_FORCE = 5.0; //força de subida do pulo enquanto ta agachado
+double JUMP_FORCE = 6.0;
 
+int DINO_HEIGTH = 44;
+int DINO_WIDTH = 48;
+
+int DINO_DUCKING_HEIGTH = 22;
+int DINO_DUCKING_WIDTH = 60;
 
 
 //----------------------------------------------------//
@@ -113,7 +134,6 @@ void Cactus::Render(){
 }
 
 vector<Cactus> cactuses;
-
 
 
 class Ptera{
@@ -180,6 +200,13 @@ vector<Ptera> pteras;
 bool InitializeSDL(){
     SDL_Init(SDL_INIT_VIDEO);
 
+    //inicializando a biblioteca de fonte(Texto)
+    if( TTF_Init() == -1 ){
+        cout << "TTF_Init Error " << TTF_GetError() << endl;
+        return false;
+    }
+
+    //inicializando a janela
     window = SDL_CreateWindow(
         "Dino Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN
@@ -191,15 +218,15 @@ bool InitializeSDL(){
         return false;
     }
 
+    //inicializando o renderer (para carregar coisas na janela)
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
     if(!renderer){
         cout << "SDL_CreateRenderer Error: " << SDL_GetError << endl;
         SDL_Quit();
         return false;
     }
 
-    //SDL_Delay(5000);
+
 
     return true;
 }   
@@ -215,11 +242,43 @@ void HandleJump(){
 }
 
 /**
+ * @brief recebe dois rect e retorna se um rect encosta no outro
+ * @param rect1 primeiro rect, ex: rect do dinossauro
+ * @param rect2 segundo rect, ex: rect do cacto ou do ptera
+ * @return true, se colidiram, false caso contrario
+*/
+bool checkCollision(SDL_Rect rect1, SDL_Rect rect2){
+    //1 caso: o rect1 esta abaixo do rect2
+    if(rect1.y + rect1.h < rect2.y){
+        return false;
+    }
+    //2 caso: o rect1 esta acima do rect2
+    else if(rect1.y > rect2.h + rect2.y){
+        return false;
+    }
+    //3 caso: o rect1 esta a esquerda do rect2
+    else if(rect1.x + rect1.w < rect2.x){
+        return false;
+    }
+    //4 caso: o rect1 esta a direita do rect2
+    else if(rect1.x > rect2.x + rect2.w){
+        return false;
+    }
+
+    //5 caso: houve colisao
+    return true;
+    
+}
+
+/**
  * @brief atualiza a coordenada Y do dinossauro no pulo
 */
 void UpdateDino(){
+
+    (isDucking) ? JUMP_FORCE = DUCKING_JUMP_FORCE : JUMP_FORCE = NORMAL_JUMP_FORCE;
+    
     //dinoY += static_cast<int>(dinoYVelocity);
-    dinoY += dinoYVelocity;
+    dinoY += (int) dinoYVelocity;
     dinoYVelocity += GRAVITY;
 
     if(dinoY >= GROUND_HEIGHT){
@@ -281,12 +340,37 @@ bool LoadMedia(){
     SDL_Surface* dinoSurface2 = IMG_Load(dinoImagePath2.c_str());
 
     if(!dinoSurface2){
-        cout << "Problem loading dino_2 .png "<< IMG_GetError() << endl;
+        cout << "Problem loading dino_2.png "<< IMG_GetError() << endl;
         return false;
     }
 
     dinoTexture2 = SDL_CreateTextureFromSurface(renderer, dinoSurface2);
     SDL_FreeSurface(dinoSurface2);
+
+    //carregando o dinossauro abaixado 1
+    string dinoDuckingImagePath = SPRITES_FOLDER + string("dino_ducking_1.png");
+    SDL_Surface* dinoDuckingSurface = IMG_Load(dinoDuckingImagePath.c_str());
+
+    if(!dinoDuckingSurface){
+        cout << "Problem loading dino_ducking_1.png "<< IMG_GetError() << endl;
+        return false;
+    }
+
+    dinoDuckingTexture = SDL_CreateTextureFromSurface(renderer, dinoDuckingSurface);
+    SDL_FreeSurface(dinoDuckingSurface);
+
+     //carregando o dinossauro abaixado 2
+    string dinoDucking2ImagePath = SPRITES_FOLDER + string("dino_ducking_2.png");
+    SDL_Surface* dinoDucking2Surface = IMG_Load(dinoDucking2ImagePath.c_str());
+
+    if(!dinoDucking2Surface){
+        cout << "Problem loading dino_ducking_2.png "<< IMG_GetError() << endl;
+        return false;
+    }
+
+    dinoDucking2Texture = SDL_CreateTextureFromSurface(renderer, dinoDucking2Surface);
+    SDL_FreeSurface(dinoDucking2Surface);
+
 
 
     //carregando o chao
@@ -325,13 +409,67 @@ bool LoadMedia(){
     ptera2Texture = SDL_CreateTextureFromSurface(renderer, ptera2Surface);
     SDL_FreeSurface(ptera2Surface);
 
+    //carregando a fonte
+    font = TTF_OpenFont(FONT_PATH, FONT_SIZE);
+    if(!font){
+        cout << "TTF_OpenFont Error " << TTF_GetError() << endl;
+        return false;
+    }
+
 
     return true;
 } 
 
+void RenderText(const char* text, SDL_Texture*& texture, SDL_Rect& rect){
+    SDL_Color textColor = {255, 0, 0}; //cor em rgb
+
+    SDL_Surface* textSurface = TTF_RenderText_Solid(font, text, textColor);
+    texture = SDL_CreateTextureFromSurface(renderer, textSurface);
+    
+    rect.w = textSurface->w;
+    rect.h = textSurface->h;
+    rect.x = (SCREEN_WIDTH - rect.w) / 2; 
+
+    SDL_FreeSurface(textSurface);
+}
+
+void RenderGameoverText(){
+    SDL_Rect gameOverRect;
+    SDL_Rect replayRect;
+
+    gameOverRect.y = SCREEN_HEIGHT/2 - 30;
+    replayRect.y = SCREEN_HEIGHT/2 + 50;
+
+    RenderText("Game Over", gameOverTexture, gameOverRect);
+    RenderText("Press Space to Replay", replayTexture, replayRect);
+
+    SDL_RenderCopy(renderer, gameOverTexture, NULL, &gameOverRect);
+    SDL_RenderCopy(renderer, replayTexture, NULL, &replayRect); 
+}
+
+void ResetGame(){
+    gameOver = false;
+
+    cactuses.clear();
+    pteras.clear();
+
+    cactuses.push_back(Cactus(SMALL_CACTUS, 800, groundSpeed)); //small cactus
+    cactuses.push_back(Cactus(BIG_CACTUS, 1200, groundSpeed)); //big cactus
+
+    ground1X = 0;
+    ground2X = ground1X + groundImageWidth; //inicializando o segundo chao no final do primeiro
+
+    dinoYVelocity = 0;
+    dinoY = GROUND_HEIGHT;
+    isJumping = false;
+    isDucking = false;
+}
+
 
 int main(int argc, char* argv[]){
-    int c = 0;
+
+    int c = 0 , d = 0; //contadores para animacao de andar
+
     if(!InitializeSDL()){
         return 1;
     }
@@ -344,12 +482,11 @@ int main(int argc, char* argv[]){
         return 1;
     }
 
-    cactuses.push_back(Cactus(SMALL_CACTUS, 800, groundSpeed)); //small cactus
-    cactuses.push_back(Cactus(BIG_CACTUS, 1200, groundSpeed)); //big cactus
+    ResetGame();
 
-    ground2X = ground1X + groundImageWidth;
     while(!quit){ //loop for game loop
-        while(SDL_PollEvent(&event) != 0){ //loop que aguarda algum evento
+
+        while(SDL_PollEvent(&event) != 0){ //loop que consome todos os eventos
             if(event.type == SDL_QUIT){
                 quit = true; 
             }
@@ -357,67 +494,115 @@ int main(int argc, char* argv[]){
                 //checa se a tela pressionada é a seta pra cima ou o espaco
                 if(event.key.keysym.sym == SDLK_UP || event.key.keysym.sym == SDLK_SPACE)
                 {
-                    HandleJump();
+                    //checa se houve gameOver
+                    if(gameOver){
+                        ResetGame();
+                    }
+                    else{ //caso contrario, lida com o pulo dodinossauro
+                        HandleJump();
+                    } 
+                }
+                else if(event.key.keysym.sym == SDLK_DOWN){
+                    isDucking = !isDucking;
                 }
             }
         }
 
-        //spawna pteras periodicamente
-        if(SDL_GetTicks() % 3000 == 0){
-            pteras.push_back(Ptera(rand() %2, SCREEN_WIDTH, rand()%100 + 100, groundSpeed));
-        }
+        if(!gameOver){ //atualiza a janela caso nao tenha dado gameOver(colisao)
 
-        for(int i = 0; i < pteras.size(); i++){
-            pteras[i].Update();
-        }
+            UpdateGround();
+            UpdateDino();
 
-        for(int i = 0; i < cactuses.size(); i++){
-            cactuses[i].Update();
-        }
-
-
-
-        UpdateGround();
-
-        UpdateDino();
-
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-
-        SDL_RenderClear(renderer); //limpa o frame atual para carregar o proximo frame
-        SDL_Rect dinoRect = {dinoX, dinoY, 44, 48};
-        
-        if(c <= 50)
-            SDL_RenderCopy(renderer, dinoTexture, NULL, &dinoRect);
-        else if(c <= 100)
-            SDL_RenderCopy(renderer, dinoTexture1, NULL, &dinoRect);
-        else 
-            SDL_RenderCopy(renderer, dinoTexture2, NULL, &dinoRect);
-        c++;
-        if(c > 150) c = 1;
-
-        SDL_Rect groundRect1 = {ground1X, GROUND_HEIGHT + 25, groundImageWidth, 40};
-        SDL_RenderCopy(renderer, groundTexture1, NULL, &groundRect1);
-
-        SDL_Rect groundRect2 = {ground2X, GROUND_HEIGHT + 25, groundImageWidth, 40};
-        SDL_RenderCopy(renderer, groundTexture2, NULL, &groundRect2);
-
-        for(int i = 0; i < cactuses.size(); i++){
-            cactuses[i].Render();
-            if(cactuses[i].GetHitbox().x + cactuses[i].GetHitbox().w < 0){
-                cactuses[i] = Cactus(rand()%2, SCREEN_WIDTH, groundSpeed);
+            //spawna pteras periodicamente
+            if(SDL_GetTicks() % 3000 == 0){
+                pteras.push_back(Ptera(rand() %2, SCREEN_WIDTH, rand()%100 + 100, groundSpeed));
             }
-        }
 
-        for(int i = 0; i < pteras.size(); i++){
-            pteras[i].Render();
-            if(pteras[i].GetHitbox().x + pteras[i].GetHitbox().w < 0){
-                pteras.erase(pteras.begin() + i);
+
+            //atualiza os ptera
+            for(int i = 0; i < pteras.size(); i++){
+                pteras[i].Update();
             }
+
+            //atualiza os cacto
+            for(int i = 0; i < cactuses.size(); i++){
+                cactuses[i].Update();
+            }
+
+            int altura_dino;
+            (isDucking) ? altura_dino = DINO_DUCKING_HEIGTH : altura_dino = DINO_HEIGTH; 
+
+             int largura_dino;
+            (isDucking) ? largura_dino = DINO_DUCKING_WIDTH : largura_dino = DINO_WIDTH; 
+
+            SDL_Rect dinoColisionRect = {dinoX, dinoY, largura_dino, altura_dino};
+            //checando colisao de cacto
+            for(int i = 0; i < cactuses.size(); i++){
+                if(checkCollision(dinoColisionRect, cactuses[i].GetHitbox())){
+                    gameOver = true;
+                }
+            }
+
+            //checando colisao de ptera
+            for(int i = 0; i < pteras.size(); i++){
+                if(checkCollision(dinoColisionRect, pteras[i].GetHitbox())){
+                    gameOver = true;
+                }
+            }
+
+
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+            SDL_RenderClear(renderer); //limpa o frame atual para carregar o proximo frame
+            
+
+            SDL_Rect dinoRect = {dinoX, dinoY, DINO_WIDTH, DINO_HEIGTH};
+            if(isDucking){
+                if(d <= 50)
+                    SDL_RenderCopy(renderer, dinoDuckingTexture, NULL, &dinoRect);
+                else if(d <= 100)
+                    SDL_RenderCopy(renderer, dinoDucking2Texture, NULL, &dinoRect);
+                d++;
+                if(d > 100) d = 1;
+            }
+
+            else{
+                if(c <= 50)
+                    SDL_RenderCopy(renderer, dinoTexture, NULL, &dinoRect);
+                else if(c <= 100)
+                    SDL_RenderCopy(renderer, dinoTexture1, NULL, &dinoRect);
+                else 
+                    SDL_RenderCopy(renderer, dinoTexture2, NULL, &dinoRect);
+                c++;
+                if(c > 150) c = 1;
+            }
+
+            SDL_Rect groundRect1 = {ground1X, GROUND_HEIGHT + 25, groundImageWidth, 40};
+            SDL_RenderCopy(renderer, groundTexture1, NULL, &groundRect1);
+
+            SDL_Rect groundRect2 = {ground2X, GROUND_HEIGHT + 25, groundImageWidth, 40};
+            SDL_RenderCopy(renderer, groundTexture2, NULL, &groundRect2);
+
+            for(int i = 0; i < cactuses.size(); i++){
+                cactuses[i].Render();
+                if(cactuses[i].GetHitbox().x + cactuses[i].GetHitbox().w < 0){
+                    cactuses[i] = Cactus(rand()%2, SCREEN_WIDTH, groundSpeed);
+                }
+            }
+
+            for(int i = 0; i < pteras.size(); i++){
+                pteras[i].Render();
+                if(pteras[i].GetHitbox().x + pteras[i].GetHitbox().w < 0){
+                    pteras.erase(pteras.begin() + i);
+                }
+            }
+        } //fim do if(!gameOver);
+
+        if(gameOver){ //se houve gameover, mostra o texto na tela
+            RenderGameoverText();
         }
-        
 
         SDL_RenderPresent(renderer);
-
         SDL_Delay(3);
     }
 
